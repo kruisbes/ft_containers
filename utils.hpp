04d15340072c743cd07d4f80389749b933727f21
Fines Raphael <fraphael@student.21-school.ru>
2021-10-06 15:44:40 +0300

    iterator traits

--- utils.hpp
+++ utils.hpp
@@ -1,5 +1,5 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
+#ifndef FT_CONTAINERS_UTILS_HPP
+#define FT_CONTAINERS_UTILS_HPP
 
 namespace ft {
 
@@ -100,6 +100,30 @@ namespace ft {
 	template<class T>
 	struct enable_if<true, T> {
 		typedef T type;
+	};
+	template<class Iter>
+	struct iterator_traits {
+		typedef typename Iter::difference_type difference_type;
+		typedef typename Iter::value_type value_type;
+		typedef typename Iter::pointer pointer;
+		typedef typename Iter::reference reference;
+		typedef typename Iter::iterator_category iterator_category;
+	};
+	template<class T>
+	struct iterator_traits<T*> {
+		typedef std::ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+	template<class T>
+	struct iterator_traits<const T*> {
+		typedef std::ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef std::random_access_iterator_tag iterator_category;
 	};
 }
 
