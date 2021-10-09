12dd0574c7280d34d6043a3a0c8609103a85ad02
Fines Raphael <fraphael@student.21-school.ru>
2021-10-09 17:08:36 +0300

    ok

--- map/pair.hpp
+++ map/pair.hpp
@@ -13,10 +13,33 @@ namespace ft {
 		T1 first;
 		T2 second;
 
-		// CONSTRUCTORS + DESTRUCTOR
+		// CONSTRUCTORS
 
-		pair() {}
+		pair() : first(), second() {}
+		pair(const T1& x, const T2& y) : first(x), second(y) {}
+		template<class U1, class U2>
+		pair(const pair<U1, U2>& p);
+
+		// OPERATOR
+
+		pair& operator=(const pair& other);
 	};
+
+	// NON-MEMBER FUNCTIONS
+	template<class T1, class T2>
+	ft::pair<T1, T2> make_pair(T1 t, T2 u);
+	template<class T1, class T2>
+	bool operator==(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
+	template<class T1, class T2>
+	bool operator!=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
+	template<class T1, class T2>
+	bool operator<(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
+	template<class T1, class T2>
+	bool operator<=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
+	template<class T1, class T2>
+	bool operator>(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
+	template<class T1, class T2>
+	bool operator>=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs);
 }
 
 #endif
